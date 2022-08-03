189330646cc899af6e94865f53e7252327cc8dd5
vndee <vndee.huynh@gmail.com>
2022-08-03 14:57:36 +0700

    update nữa nè

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='deenorm',
+    version='2.0.0',    
+    description='Python package for convert text to phoneme ipa, use for cross language embedding Text-to-speech Reseach',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['deenorm'],
+    include_package_data=True,
+    install_requires=["vinorm", "underthesea", "eng_to_ipa", "git+https://github.com/quadrismegistus/prosodic.git"],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: Free for non-commercial use',
+        'Natural Language :: Vietnamese',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],  
+)
